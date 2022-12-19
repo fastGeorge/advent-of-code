24737abd3d3165fdf81ac30a976d68eb205b61af
gustav <gustav.p.andre@gmail.com>
2022-12-19 14:34:42 +0100

    day14 sloppy animation using matplotlib.imshow

--- day14/problems14.py
+++ day14/problems14.py
@@ -1,38 +1,44 @@
-import sys
-sys.path.append("../advent-of-code")
+import sys, os, time
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from filereader.filereader import getLinesInputFile
     
 
 inputList = getLinesInputFile("day14", "input.txt")
 
-startC = 100
+startC = 300
 rL = 200
-cL = 1000
-grid = []
-for r in range(rL):
-    row = []
-    for c in range(cL):
-        if c == 0:
-            if r >= 3:
-                row.append(str(r - 3))
-            else:
-                row.append(" ")
-        else:
-            if r < 3:
-                hund = (startC + c - 1) // 100
-                ten = ((startC + c - 1) - hund * 100) // 10
-                one = (startC + c - 1) % 10
-                if r == 0:
-                    row.append(str(hund))
-                if r == 1:
-                    row.append(str(ten))
-                if r == 2:
-                    row.append(str(one))
-            else:
-                row.append(".")
-    grid.append(row)
+cL = 400
 
+def makeGrid(startC = startC, rL = rL, cL = cL):
+    grid = []
+    for r in range(rL):
+        row = []
+        for c in range(cL):
+            if c == 0:
+                if r >= 3:
+                    row.append(str(r - 3))
+                else:
+                    row.append(" ")
+            else:
+                if r < 3:
+                    hund = (startC + c - 1) // 100
+                    ten = ((startC + c - 1) - hund * 100) // 10
+                    one = (startC + c - 1) % 10
+                    if r == 0:
+                        row.append(str(hund))
+                    if r == 1:
+                        row.append(str(ten))
+                    if r == 2:
+                        row.append(str(one))
+                else:
+                    row.append(".")
+        grid.append(row)
+    
+    return grid
+
+grid = makeGrid()
 
 def printGrid(grid:list):
     for r in grid:
@@ -61,6 +67,8 @@ def readInput(line:str, startC:int, maxH:int, grid = grid):
         maxH = max(maxH, max(idx1[1] + 3, idx2[1] + 3))
     return maxH
 
+def getGrid():
+    return grid
 
 maxH = 0
 for line in inputList:
@@ -92,7 +100,7 @@ def sandPour(grid = grid):
     grid[sR][sC] = "."
     return 0
 
-def sandPour2(grid = grid):
+def sandPour2(grid:list):
     sR = 3
     sC = 500 - startC + 1
     grid[sR][sC] = "o"
@@ -116,13 +124,51 @@ def sandPour2(grid = grid):
         grid[sR][sC] = "o"
 
 
-#One added to make up for the last grain of sand
-grains = 1
-while sandPour2():
+# SLOPPY ANIMATION
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FFMpegWriter
+
+grid = getGrid()
+
+
+
+def nbrGrid(grid):
+    newGrid = []
+    for r in range(len(grid[3:])):
+        row = []
+        for c in range(len(grid[0][1:])):
+            val = None
+            if grid[r + 3][c + 1] == ".":
+                val = 0
+            elif grid[r + 3][c + 1] == "#":
+                val = 1
+            elif grid[r + 3][c + 1] == "o":
+                val = 2
+            row.append(val)
+        newGrid.append(row)
+    return newGrid
+
+
+arr = np.array(nbrGrid(grid))
+
+plt.ion()
+
+fig = plt.figure()
+
+pixel_plot = plt.imshow(arr, cmap='tab20b', vmin=0, vmax=2)
+
+plt.show()
+
+grains = 0
+while sandPour2(grid):
     grains += 1
+    if grains % 500 == 0:
+        arr = np.array(nbrGrid(grid))
+        pixel_plot.set_data(arr)
+        fig.canvas.flush_events()
 
-printGrid(grid)
+time.sleep(3)
+
 print(grains)
-
-
-
